20b27fffda590087063aea7ffed82179e2a78a8d
Eric Feng <31462296+eric-feng-2011@users.noreply.github.com>
2023-12-08 23:07:51 -0500

    Problem 8 solved

--- src/year2023/Problem8.java
+++ src/year2023/Problem8.java
@@ -1,8 +1,10 @@
 package year2023;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class Problem8 {
 
@@ -21,34 +23,25 @@ public class Problem8 {
         }
     }
 
-    public static int solve(String[] list) {
-        String currentNode = "";
+    public static long solve(String[] list) {
         for (int i = 2; i < list.length; i++) {
-            String parsedKey = parseLineAndSetNodes(list[i]);
-            if (i == 2) {
-                currentNode = parsedKey;
+            parseLineAndSetNodes(list[i]);
+        }
+
+        char[] LRInstructions = list[0].toCharArray();
+
+        // Part 1
+//        int numSteps = determineNumSteps("AAA", "ZZZ", LRInstructions);
+
+        // Part 2
+        long numSteps = 1;
+        for (String key: stringNodeMap.keySet()) {
+            if (key.charAt(2) == 'A') {
+                numSteps = lcm(numSteps, determineNumSteps(key, null, LRInstructions));
             }
         }
 
-        int numSteps = 0;
-        char[] LRInstructions = list[0].toCharArray();
-        while (true) {
-            if (currentNode.equals("ZZZ")) {
-                return numSteps;
-            }
-            char currentInstruction = LRInstructions[numSteps % (LRInstructions.length)];
-            switch (currentInstruction) {
-                case 'L':
-                    currentNode = stringNodeMap.get(currentNode).leftNodeValue;
-                    break;
-                case 'R':
-                    currentNode = stringNodeMap.get(currentNode).rightNodeValue;
-                    break;
-                default:
-                    throw new RuntimeException(String.format("Not a valid direction instruction: %c", currentInstruction));
-            }
-            numSteps += 1;
-        }
+        return numSteps;
     }
 
     private static String parseLineAndSetNodes(String line) {
@@ -64,5 +57,45 @@ public class Problem8 {
         stringNodeMap.putIfAbsent(nodeValue,
                 new Node(nodeValue, nodeDirections[0].trim(), nodeDirections[1].trim()));
         return nodeValue;
+    }
+
+    private static int determineNumSteps(String currentNode, String endingNode, char[] LRInstructions) {
+        int numSteps = 0;
+
+        while (true) {
+            if ((currentNode.equals(endingNode))
+                || (Objects.isNull(endingNode) && currentNode.charAt(2) == 'Z')) {
+                return numSteps;
+            }
+            for (char currentInstruction: LRInstructions) {
+                switch (currentInstruction) {
+                    case 'L':
+                        currentNode = stringNodeMap.get(currentNode).leftNodeValue;
+                        break;
+                    case 'R':
+                        currentNode = stringNodeMap.get(currentNode).rightNodeValue;
+                        break;
+                    default:
+                        throw new RuntimeException(String.format("Not a valid direction instruction: %c", currentInstruction));
+                }
+                numSteps += 1;
+            }
+        }
+    }
+
+    // From baeldung - LCM function
+    public static long lcm(long number1, long number2) {
+        if (number1 == 0 || number2 == 0) {
+            return 0;
+        }
+        long absNumber1 = Math.abs(number1);
+        long absNumber2 = Math.abs(number2);
+        long absHigherNumber = Math.max(absNumber1, absNumber2);
+        long absLowerNumber = Math.min(absNumber1, absNumber2);
+        long lcm = absHigherNumber;
+        while (lcm % absLowerNumber != 0) {
+            lcm += absHigherNumber;
+        }
+        return lcm;
     }
 }
